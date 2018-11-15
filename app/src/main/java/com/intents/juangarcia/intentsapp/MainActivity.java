49a0bb361294cfbbd576762ee118ef868200019a
garcajuDev <jga.ifcd0210@gmail.com>
2018-11-15 23:36:16 +0100

    Upload2 - Cámara

--- app/src/main/java/com/intents/juangarcia/intentsapp/MainActivity.java
+++ app/src/main/java/com/intents/juangarcia/intentsapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.intents.juangarcia.intentsapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView photo;
+    Button btnTakePhoto;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        photo = findViewById(R.id.imgPlaceholder);
+        camera();
+    }
+
+    public void camera(){
+        btnTakePhoto = findViewById(R.id.btnCamera);
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photoImage = (Bitmap)data.getExtras().get("data");
+        photo.setImageBitmap(photoImage);
     }
 }
