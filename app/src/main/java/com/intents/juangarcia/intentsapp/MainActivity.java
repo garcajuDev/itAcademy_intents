23c8b279bf523b1420539038f51bcbe725905b24
garcajuDev <jga.ifcd0210@gmail.com>
2018-11-16 00:30:58 +0100

    Upload3 - Galería y refactoriazación

--- app/src/main/java/com/intents/juangarcia/intentsapp/MainActivity.java
+++ app/src/main/java/com/intents/juangarcia/intentsapp/MainActivity.java
@@ -2,42 +2,49 @@ package com.intents.juangarcia.intentsapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    final int CAMERA = 1;
+    final int GALLERY = 2;
     ImageView photo;
-    Button btnTakePhoto;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         photo = findViewById(R.id.imgPlaceholder);
-        camera();
     }
 
-    public void camera(){
-        btnTakePhoto = findViewById(R.id.btnCamera);
-        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, 0);
-            }
-        });
+    public void camera(View view){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA);
+    }
+
+    public void photoGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Selecciona la Aplicación"),GALLERY);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap photoImage = (Bitmap)data.getExtras().get("data");
-        photo.setImageBitmap(photoImage);
+
+        if (requestCode == CAMERA && resultCode == RESULT_OK) {
+            Bitmap photoImage = (Bitmap) data.getExtras().get("data");
+            photo.setImageBitmap(photoImage);
+        }
+        if (requestCode == GALLERY && resultCode == RESULT_OK){
+            Uri path = data.getData();
+            photo.setImageURI(path);
+        }
     }
 }
